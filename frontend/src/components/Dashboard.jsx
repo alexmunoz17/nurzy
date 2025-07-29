231a85fa3ddb97a7df21138519105c29ca4b0fb6
amunoz-pdc <amunoz@pdc-online.com>
2025-07-29 16:34:28 +0200

    Dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import './Sidebar.css'
+import './Dashboard.css'
 
 function Dashboard() {
     const [pageIsLoaded, setPageIsLoaded] = useState(false);
@@ -19,12 +19,42 @@ function Dashboard() {
 
     return (
         <>
-            <div className="flex-grow-1 p-3 bg-light vh-100" style={ animationStyle }>
+            <div className="flex-grow-1 p-3 ps-5 pe-5 bg-light vh-100" style={ animationStyle }>
                 <h2>Dashboard</h2>
                 <div className="flex-row w-100">
                     <div className="row mb-4">
-                        <div className="col-sm-12 mb-3 mb-sm-0">
-                            <div className="card">
+                        <div className="col-sm-3 mb-3 mb-sm-0">
+                            <div className="card nurzy-card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Special title treatment</h5>
+                                    <p className="card-text">With supporting text below as a natural lead-in to
+                                        additional content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3 mb-3 mb-sm-0">
+                            <div className="card nurzy-card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Special title treatment</h5>
+                                    <p className="card-text">With supporting text below as a natural lead-in to
+                                        additional content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3 mb-3 mb-sm-0">
+                            <div className="card nurzy-card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Special title treatment</h5>
+                                    <p className="card-text">With supporting text below as a natural lead-in to
+                                        additional content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3 mb-3 mb-sm-0">
+                            <div className="card nurzy-card">
                                 <div className="card-body">
                                     <h5 className="card-title">Special title treatment</h5>
                                     <p className="card-text">With supporting text below as a natural lead-in to
