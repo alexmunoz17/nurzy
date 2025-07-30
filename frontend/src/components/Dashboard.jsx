68cf07b5d54a63c9a60427fe3394d2303db4d566
amunoz-pdc <amunoz@pdc-online.com>
2025-07-30 16:09:56 +0200

    Sidebar & Dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -26,15 +26,24 @@ function Dashboard() {
                         <div className="col-sm-3 mb-3 mb-sm-0">
                             <div className="card nurzy-card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to
-                                        additional content.</p>
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    <div className="row">
+                                        <div className="col-8">
+                                            <p className="text-sm mb-0 text-uppercase font-weight-bold">Open applications</p>
+                                            <h5 className="font-weight-bolder">4</h5>
+                                            <p className="mb-0">Waiting for answer</p>
+                                        </div>
+                                        <div className="col-4">
+                                            <div
+                                                className="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                                <i className="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm-3 mb-3 mb-sm-0">
-                            <div className="card nurzy-card">
+                        <div className="card nurzy-card">
                                 <div className="card-body">
                                     <h5 className="card-title">Special title treatment</h5>
                                     <p className="card-text">With supporting text below as a natural lead-in to
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ function Sidebar({ selectedPage, onSelectPage }) {
     return (
         <>
             <div className="p-3 vh-100 d-flex flex-column navbar-sidebar d-none d-md-flex"
-                 style={{ width: collapsedSidebar ? "5rem" : "16rem" }}>
+                 style={{ minWidth: collapsedSidebar ? "5rem" : "16rem" }}>
                 <div className="d-flex align-items-center">
                     <h4 className="mb-0 heading-sidebar">{collapsedSidebar ? "N" : "Nurzy"}</h4>
                     <h4 className="ms-auto mb-0 icon-collapse">
@@ -27,43 +27,43 @@ function Sidebar({ selectedPage, onSelectPage }) {
                     {collapsedSidebar ? null : (<h5 className="mb-0 ms-2">Alex</h5>)}
                 </div>
                 <hr/>
-                <button className={`btn btn-sidebar p-2
-                                    ${collapsedSidebar ? "collapsed d-flex" : ""}
+                <button className={`btn btn-sidebar p-2 d-flex align-items-center
+                                    ${collapsedSidebar ? "collapsed" : ""}
                                     ${selectedPage === "dashboard" ? "btn-sidebar-active" : ""}`}
                         onClick={() => onSelectPage("dashboard")}>
-                    <i className="bi bi-bar-chart"></i> {collapsedSidebar ? null : "Dashboard"}
+                    <i className="bi bi-bar-chart"></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">Dashboard</h5>}
                 </button>
-                <button className={`btn btn-sidebar p-2
-                                    ${collapsedSidebar ? "collapsed d-flex" : ""}
+                <button className={`btn btn-sidebar p-2 d-flex align-items-center
+                                    ${collapsedSidebar ? "collapsed" : ""}
                                     ${selectedPage === "jobs" ? "btn-sidebar-active" : ""}`}
                         onClick={() => onSelectPage("jobs")}>
-                    <i className="bi bi-briefcase"></i> {collapsedSidebar ? null : "Jobs"}
+                    <i className="bi bi-briefcase"></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">Jobs</h5>}
                 </button>
-                <button className={`btn btn-sidebar p-2
-                                    ${collapsedSidebar ? "collapsed d-flex" : ""}
+                <button className={`btn btn-sidebar p-2 d-flex align-items-center
+                                    ${collapsedSidebar ? "collapsed" : ""}
                                     ${selectedPage === "caredits" ? "btn-sidebar-active" : ""}`}
                         onClick={() => onSelectPage("caredits")}>
-                    <i className="bi bi-award"></i> {collapsedSidebar ? null : "Caredits"}
+                    <i className="bi bi-award"></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">Caredits</h5>}
                 </button>
-                <button className={`btn btn-sidebar p-2
-                                    ${collapsedSidebar ? "collapsed d-flex" : ""}
+                <button className={`btn btn-sidebar p-2 d-flex align-items-center
+                                    ${collapsedSidebar ? "collapsed" : ""}
                                     ${selectedPage === "profile" ? "btn-sidebar-active" : ""}`}
                         onClick={() => onSelectPage("profile")}>
-                    <i className="bi bi-person"></i> {collapsedSidebar ? null : "Profile"}
+                    <i className="bi bi-person"></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">Profile</h5>}
                 </button>
                 <div className="d-flex flex-column mt-auto">
                     <hr/>
-                    <button className={`btn btn-sidebar p-2 mt-2
-                                        ${collapsedSidebar ? "collapsed d-flex" : ""}`}>
-                        <i className="bi bi-chevron-double-up"></i> {collapsedSidebar ? null : "Upgrade to Premium"}
+                    <button className={`btn btn-sidebar p-2 mt-2 d-flex align-items-center
+                                        ${collapsedSidebar ? "collapsed" : ""}`}>
+                        <i className="bi bi-chevron-double-up"></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">Upgrade to Premium</h5>}
                     </button>
-                    <button className={`btn btn-sidebar p-2 mt-2
-                                        ${collapsedSidebar ? "collapsed d-flex" : ""}`}>
-                        <i className="bi bi-question-circle"></i> {collapsedSidebar ? null : "Contact support"}
+                    <button className={`btn btn-sidebar p-2 mt-2 d-flex align-items-center
+                                        ${collapsedSidebar ? "collapsed" : ""}`}>
+                        <i className="bi bi-question-circle"></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">Contact support</h5>}
                     </button>
-                    <button className={`btn btn-sidebar btn-logout p-2 mt-2
-                                        ${collapsedSidebar ? "collapsed d-flex" : ""}`}>
-                        <i className="bi bi-box-arrow-right"></i> {collapsedSidebar ? null : "Log out"}
+                    <button className={`btn btn-sidebar btn-logout p-2 mt-2 d-flex align-items-center
+                                        ${collapsedSidebar ? "collapsed" : ""}`}>
+                        <i className="bi bi-box-arrow-right"></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">Log out</h5>}
                     </button>
                 </div>
             </div>
