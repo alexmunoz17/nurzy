0659ba62cb9ed09805f7d96cd1ff7acb8c1b0828
amunoz-pdc <amunoz@pdc-online.com>
2025-09-03 16:40:10 +0200

    Enhanced Sidebar styling and spacing adjustments

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -12,22 +12,20 @@ function Sidebar() {
     };
 
     return (
-        <div className="p-3 vh-100 d-flex flex-column navbar-sidebar d-none d-md-flex flex-shrink-0"
-             style={{ width: collapsedSidebar ? "5rem" : "16rem" }}>
-            <div className="d-flex align-items-center">
-                <h4 className="mb-0 heading-sidebar">{collapsedSidebar ? "N" : "Nurzy"}</h4>
+        <div className="p-4 vh-100 d-flex flex-column navbar-sidebar d-none d-md-flex flex-shrink-0"
+             style={{ width: collapsedSidebar ? "5rem" : "20rem" }}>
+            <div className="d-flex pb-4 align-items-center">
+                <span className="mb-0 heading-sidebar">{collapsedSidebar ? "N" : "Nurzy"}</span>
                 <h4 className="ms-auto mb-0 icon-collapse">
                     <i className={`bi ${collapsedSidebar ? "bi-arrow-right" : "bi-arrow-left"}`} onClick={toggleSidebar}></i>
                 </h4>
             </div>
-            <hr/>
-            <div className={`d-flex align-items-center ${collapsedSidebar ? "collapsed d-flex" : ""}`}>
+            <div className={`d-flex pb-4 align-items-center ${collapsedSidebar ? "collapsed d-flex" : ""}`}>
                 <div className="icon-profile">
                     <i className="bi bi-person"></i>
                 </div>
                 {collapsedSidebar ? null : (<h5 className="mb-0 ms-2">Alex</h5>)}
             </div>
-            <hr/>
             <SidebarLink url="/dashboard" icon="bi bi-bar-chart" label="Dashboard" collapsedSidebar={collapsedSidebar} />
             <SidebarLink url="/jobs" icon="bi bi-briefcase" label="Jobs" collapsedSidebar={collapsedSidebar} />
             <SidebarLink url="/caredits" icon="bi bi-award" label="Caredits" collapsedSidebar={collapsedSidebar} />
--- frontend/src/components/Sidebar/SidebarLink.jsx
+++ frontend/src/components/Sidebar/SidebarLink.jsx
@@ -6,10 +6,10 @@ function SidebarLink({ url, icon, label, collapsedSidebar }) {
 
     return (
         <>
-            <Link to={url} className={`btn btn-sidebar p-2 d-flex align-items-center
+            <Link to={url} className={`btn btn-sidebar p-3 d-flex align-items-center
                                 ${collapsedSidebar ? "collapsed" : ""}
                                 ${currentPath === url.substring(1) ? "btn-sidebar-active" : ""}`}>
-                <i className={`${icon}`}></i> {collapsedSidebar ? null : <h5 className="mb-0 ms-1">{label}</h5>}
+                <i className={`${icon}`}></i> {collapsedSidebar ? null : <span className="mb-0 ms-1">{label}</span>}
             </Link>
         </>
     )
