bbb6ab78dc9909728147285cc3c714d3e2ea5fbb
amunoz-pdc <amunoz@pdc-online.com>
2025-09-17 09:21:26 +0200

    Conditionally hide Sidebar on the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
+import { useLocation } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 import AppRoutes from "./routes/AppRoutes.jsx";
 
 
 function App() {
+    const location = useLocation();
+    const hideSidebar = location.pathname === "/";
+
     return (
         <>
             <Navbar/>
             <div className="d-flex flex-row">
-                <Sidebar/>
+                {!hideSidebar && <Sidebar/>}
                 <AppRoutes/>
             </div>
         </>
